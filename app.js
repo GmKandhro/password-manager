ac19310bd5eb2d104d6376000a6e52f4cef616be
Gm kandhro <gmkandhro182@gmail.com>
2023-09-28 12:21:30 +0500

    password-manager for saving your password

--- app.js
+++ app.js
@@ -1,114 +1,110 @@
-let saveBtn = document.querySelector('#saveBtn')
- let card = document.querySelector('#card')
+// Get DOM elements
+const saveBtn = document.querySelector('#saveBtn');
+const card = document.querySelector('#card');
+const websiteNameInput = document.querySelector('#website-name');
+const userNameInput = document.querySelector('#userName');
+const passwordInput = document.querySelector('#password');
 
- function togglePasswordVisibility(index) {
+// Toggle password visibility
+function togglePasswordVisibility(index) {
   const passwordElement = document.getElementById(`password-${index}`);
   const eyeIcon = document.querySelector(`.toggle-eye:nth-child(${index + 1})`);
 
-  let toggleEye = document.querySelector(`.toggle-eye${index}`)
   if (passwordElement.classList.contains("visible")) {
     passwordElement.textContent = "*".repeat(passwordElement.textContent.length);
     passwordElement.classList.remove("visible");
-    toggleEye.classList.remove("fa-eye-slash");
-    toggleEye.classList.add("fa-eye");
-    
+    eyeIcon.classList.remove("fa-eye-slash");
+    eyeIcon.classList.add("fa-eye");
   } else {
     passwordElement.textContent = passwordElement.dataset.originalPassword;
     passwordElement.classList.add("visible");
-    toggleEye.classList.remove("fa-eye");
-    toggleEye.classList.add("fa-eye-slash")
+    eyeIcon.classList.remove("fa-eye");
+    eyeIcon.classList.add("fa-eye-slash");
   }
 }
 
+// Delete a card
 const deleteCard = (index) => {
-  let data = localStorage.getItem('storedData');
-  let newData = JSON.parse(data);
+  let storedData = JSON.parse(localStorage.getItem('storedData'));
 
-  if (index >= 0 ) {
-    newData.splice(index, 1); // Remove one element at the specified index
-    localStorage.setItem('storedData', JSON.stringify(newData)); // Update localStorage
+  if (index >= 0 && storedData) {
+    storedData.splice(index, 1); // Remove one element at the specified index
+    localStorage.setItem('storedData', JSON.stringify(storedData)); // Update localStorage
     displayData();
   } else {
     console.error('Invalid index to remove');
   }
 };
 
-const copyPassword = (index)=>{
+// Copy password to clipboard
+const copyPassword = (index) => {
   const passwordElement = document.getElementById(`password-${index}`);
-  let password = passwordElement.textContent = passwordElement.dataset.originalPassword;
-  console.log(password)
-  navigator.clipboard.writeText(password).then(()=>{
-    alert('password has been copied')
-  }).catch((err)=>{
-    alert('password has not been copied bue to this' , err)
-  })
-}
+  const password = passwordElement.textContent;
 
+  navigator.clipboard.writeText(password)
+    .then(() => {
+      alert('Password has been copied');
+    })
+    .catch((err) => {
+      alert('Password has not been copied due to an error: ' + err);
+    });
+};
 
-
+// Add event listener for the save button
 saveBtn.addEventListener('click', () => {
-    let webName = document.querySelector('#website-name').value;
-    let userName = document.querySelector('#userName').value;
-    let password = document.querySelector('#password').value;
+  const webName = websiteNameInput.value;
+  const userName = userNameInput.value;
+  const password = passwordInput.value;
 
-  let existingDataJSON  = localStorage.getItem('storedData')
+  let storedData = JSON.parse(localStorage.getItem('storedData')) || [];
 
-  let existingData  = existingDataJSON ? JSON.parse(existingDataJSON) : [];
+  const newObj = {
+    webName: webName,
+    userName: userName,
+    password: password,
+    originalPassword: password,
+  };
 
-  let newObj = {
-    webName : webName,
-    userName:userName,
-    password:password
-  }
+  storedData.push(newObj);
 
-  newObj.originalPassword = password;
+  localStorage.setItem('storedData', JSON.stringify(storedData));
 
-  existingData.push(newObj)
+  // Clear input fields
+  websiteNameInput.value = '';
+  userNameInput.value = '';
+  passwordInput.value = '';
 
-  let updatedDataJSON  = JSON.stringify(existingData)
-
-  localStorage.setItem('storedData' , updatedDataJSON)
-
-      document.querySelector('#website-name').value = '';
-      document.querySelector('#userName').value = '';
-      document.querySelector('#password').value = '';
-
-      displayData()
-    //   console.log(data)
-    
+  displayData();
 });
 
-let displayData = () => {
-  let data = localStorage.getItem('storedData');
-  if (data) {
-    let dataArray = JSON.parse(data); // Parse the data string into an array
-    console.log(dataArray);
+// Display stored data
+const displayData = () => {
+  let storedData = JSON.parse(localStorage.getItem('storedData')) || [];
+  card.innerHTML = '';
 
-    // Clear the existing cards
-    card.innerHTML = '';
-
-    dataArray.forEach((obj, index) => {
-      let el = `<div class="mt-2 w-[300px] h-auto rounded-2xl bg-[#d7d9df] mx-auto px-3 py-1">    
-      <h3>Website name is ${obj.webName}</h3>
-      <h3>User name is username ${obj.userName}</h3>
-      <h3>Password: 
-        <span id="password-${index}" class="password" data-original-password="${obj.password}">${"*".repeat(obj.password.length)}</span>
-        <i class="fa fa-eye toggle-eye${index}" onclick="togglePasswordVisibility(${index})"></i>
-        <i class="fas fa-copy copy-icon" onclick="copyPassword(${index})"></i> 
-      </h3>
-      <button class="bg-red p-2 mt-2" onclick="deleteCard(${index})">Delete</button>
-    </div>`;
-
-      card.insertAdjacentHTML("beforeend", el);
-    });
+  if (storedData.length === 0) {
+    card.innerHTML = '<p>Save your passwords here...</p>';
+    return;
   }
+
+  storedData.forEach((obj, index) => {
+    const el = `
+      <div class="mt-2 w-[300px] h-auto rounded-2xl bg-[#d7d9df] mx-auto px-3 py-1">    
+        <h3>Website name is ${obj.webName}</h3>
+        <h3>User name is username ${obj.userName}</h3>
+        <h3>Password: 
+          <span id="password-${index}" class="password" data-original-password="${obj.password}">
+            ${"*".repeat(obj.password.length)}
+          </span>
+          <i class="fa fa-eye toggle-eye${index}" onclick="togglePasswordVisibility(${index})"></i>
+          <i class="fas fa-copy copy-icon" onclick="copyPassword(${index})"></i> 
+        </h3>
+        <button class="bg-red p-2 mt-2" onclick="deleteCard(${index})">Delete</button>
+      </div>
+    `;
+
+    card.insertAdjacentHTML("beforeend", el);
+  });
 };
-if (card.innerHTML == '\n         \n        ') {
-  card.innerHTML = `<p>Save your pass here...</p>`
-}
-console.log(card.innerHTML)
 
 displayData();
-
-
-
